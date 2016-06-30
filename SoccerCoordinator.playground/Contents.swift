76b1751b99383093637e04560bae2d7b66560a41
serenaChan <serena@100.72.219.242>
2016-06-30 21:33:38 +0800

    Final version

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -3,6 +3,10 @@ import UIKit
 
 var str = "Hello, playground"
 
+/*Part 1: 
+We have provided information for the 18 players in the Player Info spreadsheet. Please choose an appropriate data type to store the information for each player. In addition, create an empty collection variable to hold all the players’ data. Once you have decided on what tools to use, manually enter the player data so it can be used in Part 2.
+*/
+
 let playerName : [Int: String] = [
     1: "Joe Smith",
     2: "Jill Tanner",
@@ -94,13 +98,15 @@ var experiencedPlayers: [Int] = []
 var nonExperiencedPlayers: [Int] = []
 
 
-/* Part 2*/
+/* Part 2:
+Create logic that can iterate through all 18 players and assign them to teams such that the number of experienced players on each team are the same. Store each team’s players in its own new collection variable for use in Part 3. (Please note: your logic should work correctly regardless of the initial ordering of the players and should work, if we theoretically had more or less than 18 players, so NO MAGIC NUMBERS!)
+*/
 
 
-//Each team is modeled by a dictionary storing players' index and experience status
-var teamDragons : [Int: String] = [:]
-var teamSharks : [Int: String] = [:]
-var teamRaptors : [Int: String] = [:]
+//Each team is modeled by an array storing the player indexes
+var teamDragons : [Int] = []
+var teamSharks : [Int] = []
+var teamRaptors : [Int] = []
 
 //ensures players are distributed evenly; all teams will have same number of players
 let totalPlayerInTeam = playerSoccerExp.count/3
@@ -118,8 +124,6 @@ for (index,experience) in playerSoccerExp {
 let expPlayersPerTeam = numExpPlayers/3
 
 
-/*//temp playerSoccerExp
-var tempPlayerSoccerExp = playerSoccerExp*/
 
 //updates array of experienced players and non experienced
 for(index,experience) in playerSoccerExp{
@@ -136,18 +140,24 @@ for(index,experience) in playerSoccerExp{
 
 
 //add the experiencedPlayers, according to the number of expPlayersPerTeam into teamDragons
-for i in 1...expPlayersPerTeam { //1 to 3 in this case. 1,2,3 so loops 3 times
-    teamDragons[i] = experiencedPlayers[i] //teamDragons[1] = "Joe Smith"
-    teamRaptors[i] = experiencedPlayers[i] //teamRaptors[8]= "Karl Zargyan"
-    teamSharks[i] = experiencedPlayers[i]
+
+for i in 0..<expPlayersPerTeam { //0 to 2 in this case. 0,1,2 so loops 3 times
+    
+    teamDragons.append(experiencedPlayers[i])//teamDragons[0] = 1 (player index is the value)
+    teamSharks.append(experiencedPlayers[i])//teamSharks[0] = 2
+    teamRaptors.append(experiencedPlayers[i])//teamRaptors[0] = 3
+
 }
 
 //add the remaining nonExperiencedPlayers into the teams
-for i1 in expPlayersPerTeam...totalPlayerInTeam{ //3 to 6 in this case
-    teamDragons[i1] = nonExperiencedPlayers[i1] //teamDragons[1] = "Joe Smith"
-    teamRaptors[i1] = nonExperiencedPlayers[i1]
-    teamSharks[i1] = nonExperiencedPlayers[i1]
+for i1 in expPlayersPerTeam..<totalPlayerInTeam{ //3 to 6. 3,4,5 so loops 3 times
+     teamDragons.append(nonExperiencedPlayers[i1]) //teamDragons[3] = 4
+     teamSharks.append(nonExperiencedPlayers[i1]) //teamSharks[3] = 5
+     teamRaptors.append(nonExperiencedPlayers[i1]) //teamRaptors[3] = 6
 }
+
+
+
 
 
 /*Part 3
@@ -155,17 +165,17 @@ Create logic that iterates through all three teams of players and generates a pe
 */
 
 
-for (index,name) in teamDragons{
-    print("To: \(playerGuardianName[index]), Your child \(playerName[index]) from Team Dragon will attend their first team practice on March 17, 1pm")
+for playerIndex in teamDragons{
+    print("To: \(playerGuardianName[playerIndex]), Your child \(playerName[playerIndex]) from Team Dragon will attend their first team practice on March 17, 1pm")
 }
 
 
-for (index,name) in teamSharks{
-    print("To: \(playerGuardianName[index]), Your child \(playerName[index]) from Team Sharks will attend their first team practice on March 17, 3pm")
+for playerIndex in teamSharks{
+    print("To: \(playerGuardianName[playerIndex]), Your child \(playerName[playerIndex]) from Team Sharks will attend their first team practice on March 17, 3pm")
 }
 
-for (index,name) in teamRaptors{
-    print("To: \(playerGuardianName[index]), Your child \(playerName[index]) from Team Raptors will attend their first team practice on March 18, 1pm")
+for playerIndex in teamRaptors{
+    print("To: \(playerGuardianName[playerIndex]), Your child \(playerName[playerIndex]) from Team Raptors will attend their first team practice on March 18, 1pm")
 }
 
 
